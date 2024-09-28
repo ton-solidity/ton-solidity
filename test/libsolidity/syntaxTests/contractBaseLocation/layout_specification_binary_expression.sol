25057aba16623bbb0239e8208e51e478260ed24b
Matheus Aguiar <matheus.pit@gmail.com>
2024-09-27 21:32:49 -0300

    Add tests

---
+++ test/libsolidity/syntaxTests/contractBaseLocation/layout_specification_binary_expression.sol
@@ -0,0 +1,4 @@
+contract C layout at 0xffff * (0x123 + 0xABC) { }
+// ====
+// stopAfter: parsing
+// ----
---
+++ test/libsolidity/syntaxTests/contractBaseLocation/layout_specification_constant_in_expression.sol
@@ -0,0 +1,5 @@
+uint constant X = 42;
+contract C layout at 0xffff * (50 - X) { }
+// ====
+// stopAfter: parsing
+// ----
