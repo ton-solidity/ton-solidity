ethereum/solidity

Introduce sub-assembly offset output artifact

libevmasm/Assembly.cpp
@@ -1065,6 +1065,7 @@ LinkerObject const& Assembly::assembleLegacy() const
 
 	size_t subTagSize = 1;
 	std::map<u256, LinkerObject::ImmutableRefs> immutableReferencesBySub;
+	std::vector<LinkerObject> subAssemblies;
 	for (auto const& sub: m_subs)
 	{
 		auto const& linkerObject = sub->assemble();
@@ -1077,6 +1078,7 @@ LinkerObject const& Assembly::assembleLegacy() const
 			);
 			immutableReferencesBySub = linkerObject.immutableReferences;
 		}
+		subAssemblies.push_back(linkerObject);
 		for (size_t tagPos: sub->m_tagPositionsInBytecode)
 			if (tagPos != std::numeric_limits<size_t>::max() && numberEncodingSize(tagPos) > subTagSize)
 				subTagSize = numberEncodingSize(tagPos);
@@ -1327,6 +1329,16 @@ LinkerObject const& Assembly::assembleLegacy() const
 		bytesRef r(ret.bytecode.data() + pos, bytesPerDataRef);
 		toBigEndian(ret.bytecode.size(), r);
 	}
+
+	std::vector<LinkerObject::SubAssembly> nestedSubAssemblies{};
+	for (auto const& subAssembly: subAssemblies)
+		nestedSubAssemblies.insert(nestedSubAssemblies.end(), subAssembly.subAssemblyData.begin(), subAssembly.subAssemblyData.end());
+
+	size_t const currentBytecodeSize = ret.bytecode.size();
+	updateSubAssemblyStartOffsets(nestedSubAssemblies, currentBytecodeSize);
+
+	ret.subAssemblyData.push_back({0, ret.bytecode.size(), isCreation(), ret.toHex(), nestedSubAssemblies});
+
 	return ret;
 }
 
@@ -1542,4 +1554,13 @@ Assembly::OptimiserSettings Assembly::OptimiserSettings::translateSettings(front
 	asmSettings.expectedExecutionsPerDeployment = _settings.expectedExecutionsPerDeployment;
 	asmSettings.evmVersion = _evmVersion;
 	return asmSettings;
+}
+
+void Assembly::updateSubAssemblyStartOffsets(std::vector<LinkerObject::SubAssembly>& subAssemblies, size_t const currentBytecodeSize) const
+{
+	for (auto& subAssembly : subAssemblies)
+	{
+		subAssembly.start = currentBytecodeSize - subAssembly.length;
+		updateSubAssemblyStartOffsets(subAssembly.subs, currentBytecodeSize);
+	}
 }

libevmasm/Assembly.h
@@ -253,6 +253,8 @@ private:
 	[[nodiscard]] bytes assemblePushDeployTimeAddress() const;
 	[[nodiscard]] bytes assembleTag(AssemblyItem const& _item, size_t _pos, bool _addJumpDest) const;
 
+	void updateSubAssemblyStartOffsets(std::vector<LinkerObject::SubAssembly>& subAssemblies, size_t const currentBytecodeSize) const;
+
 protected:
 	/// 0 is reserved for exception
 	unsigned m_usedTags = 1;

libsolidity/interface/StandardCompiler.cpp
@@ -237,7 +237,7 @@ bool isArtifactRequested(Json const& _outputSelection, std::string const& _file,
 std::vector<std::string> evmObjectComponents(std::string const& _objectKind)
 {
 	solAssert(_objectKind == "bytecode" || _objectKind == "deployedBytecode", "");
-	std::vector<std::string> components{"", ".object", ".opcodes", ".sourceMap", ".functionDebugData", ".generatedSources", ".linkReferences"};
+	std::vector<std::string> components{"", ".object", ".opcodes", ".sourceMap", ".functionDebugData", ".generatedSources", ".linkReferences", ".subAssemblyOffsets"};
 	if (_objectKind == "deployedBytecode")
 		components.push_back(".immutableReferences");
 	return util::applyMap(components, [&](auto const& _s) { return "evm." + _objectKind + _s; });
@@ -374,6 +374,24 @@ Json formatImmutableReferences(std::map<u256, evmasm::LinkerObject::ImmutableRef
 	return ret;
 }
 
+Json formatSubAssemblyOffsets(std::vector<evmasm::LinkerObject::SubAssembly> const& _subAssemblyOffsets)
+{
+	Json subs = Json::array();
+
+	for (auto const& subAssembly : _subAssemblyOffsets)
+	{
+		Json subAssemblyInfo;
+		subAssemblyInfo["start"] = Json::number_unsigned_t(subAssembly.start);
+		subAssemblyInfo["length"] = Json::number_unsigned_t(subAssembly.length);
+		subAssemblyInfo["isCreation"] = Json::boolean_t(subAssembly.isCreation);
+		if (!subAssembly.subs.empty())
+			subAssemblyInfo["subs"] = formatSubAssemblyOffsets(subAssembly.subs);
+		subs.emplace_back(subAssemblyInfo);
+	}
+
+	return subs;
+}
+
 Json collectEVMObject(
 	langutil::EVMVersion _evmVersion,
 	evmasm::LinkerObject const& _object,
@@ -398,6 +416,12 @@ Json collectEVMObject(
 		output["immutableReferences"] = formatImmutableReferences(_object.immutableReferences);
 	if (_artifactRequested("generatedSources"))
 		output["generatedSources"] = std::move(_generatedSources);
+	if (_artifactRequested("subAssemblyOffsets"))
+	{
+		Json ret = Json::object();
+		ret["subs"] = formatSubAssemblyOffsets(_object.subAssemblyData);
+		output["subAssemblyOffsets"] = std::move(ret);
+	}
 	return output;
 }
 

ton-solidity/ton-solidity

libevmasm/Assembly.cpp
@@ -1203,6 +1203,7 @@ LinkerObject const& Assembly::assembleLegacy() const
 
 	size_t subTagSize = 1;
 	std::map<u256, LinkerObject::ImmutableRefs> immutableReferencesBySub;
+	std::vector<LinkerObject> subAssemblies;
 	for (auto const& sub: m_subs)
 	{
 		auto const& linkerObject = sub->assemble();
@@ -1215,6 +1216,7 @@ LinkerObject const& Assembly::assembleLegacy() const
 			);
 			immutableReferencesBySub = linkerObject.immutableReferences;
 		}
+		subAssemblies.push_back(linkerObject);
 		for (size_t tagPos: sub->m_tagPositionsInBytecode)
 			if (tagPos != std::numeric_limits<size_t>::max() && numberEncodingSize(tagPos) > subTagSize)
 				subTagSize = numberEncodingSize(tagPos);
@@ -1465,6 +1467,16 @@ LinkerObject const& Assembly::assembleLegacy() const
 		bytesRef r(ret.bytecode.data() + pos, bytesPerDataRef);
 		toBigEndian(ret.bytecode.size(), r);
 	}
+
+	std::vector<LinkerObject::SubAssembly> nestedSubAssemblies{};
+	for (auto const& subAssembly: subAssemblies)
+		nestedSubAssemblies.insert(nestedSubAssemblies.end(), subAssembly.subAssemblyData.begin(), subAssembly.subAssemblyData.end());
+
+	size_t const currentBytecodeSize = ret.bytecode.size();
+	updateSubAssemblyStartOffsets(nestedSubAssemblies, currentBytecodeSize);
+
+	ret.subAssemblyData.push_back({0, ret.bytecode.size(), isCreation(), ret.toHex(), nestedSubAssemblies});
+
 	return ret;
 }
 
@@ -1775,4 +1787,13 @@ Assembly::OptimiserSettings Assembly::OptimiserSettings::translateSettings(front
 	asmSettings.expectedExecutionsPerDeployment = _settings.expectedExecutionsPerDeployment;
 	asmSettings.evmVersion = _evmVersion;
 	return asmSettings;
+}
+
+void Assembly::updateSubAssemblyStartOffsets(std::vector<LinkerObject::SubAssembly>& _subAssemblies, size_t const _currentBytecodeSize) const
+{
+	for (auto& subAssembly: _subAssemblies)
+	{
+		subAssembly.start = _currentBytecodeSize - subAssembly.length;
+		updateSubAssemblyStartOffsets(subAssembly.subs, _currentBytecodeSize);
+	}
 }

libevmasm/Assembly.h
@@ -287,6 +287,8 @@ private:
 	[[nodiscard]] bytes assemblePushDeployTimeAddress() const;
 	[[nodiscard]] bytes assembleTag(AssemblyItem const& _item, size_t _pos, bool _addJumpDest) const;
 
+	void updateSubAssemblyStartOffsets(std::vector<LinkerObject::SubAssembly>& _subAssemblies, size_t const _currentBytecodeSize) const;
+
 protected:
 	/// 0 is reserved for exception
 	unsigned m_usedTags = 1;

libsolidity/interface/StandardCompiler.cpp
@@ -237,7 +237,7 @@ bool isArtifactRequested(Json const& _outputSelection, std::string const& _file,
 std::vector<std::string> evmObjectComponents(std::string const& _objectKind)
 {
 	solAssert(_objectKind == "bytecode" || _objectKind == "deployedBytecode", "");
-	std::vector<std::string> components{"", ".object", ".opcodes", ".sourceMap", ".functionDebugData", ".generatedSources", ".linkReferences"};
+	std::vector<std::string> components{"", ".object", ".opcodes", ".sourceMap", ".functionDebugData", ".generatedSources", ".linkReferences", ".subAssemblyOffsets"};
 	if (_objectKind == "deployedBytecode")
 		components.push_back(".immutableReferences");
 	return util::applyMap(components, [&](auto const& _s) { return "evm." + _objectKind + _s; });
@@ -372,6 +372,24 @@ Json formatImmutableReferences(std::map<u256, evmasm::LinkerObject::ImmutableRef
 	}
 
 	return ret;
+}
+
+Json formatSubAssemblyOffsets(std::vector<evmasm::LinkerObject::SubAssembly> const& _subAssemblyOffsets)
+{
+	Json subs = Json::array();
+
+	for (auto const& subAssembly: _subAssemblyOffsets)
+	{
+		Json subAssemblyInfo;
+		subAssemblyInfo["start"] = Json::number_unsigned_t(subAssembly.start);
+		subAssemblyInfo["length"] = Json::number_unsigned_t(subAssembly.length);
+		subAssemblyInfo["isCreation"] = Json::boolean_t(subAssembly.isCreation);
+		if (!subAssembly.subs.empty())
+			subAssemblyInfo["subs"] = formatSubAssemblyOffsets(subAssembly.subs);
+		subs.emplace_back(subAssemblyInfo);
+	}
+
+	return subs;
 }
 
 std::optional<Json> checkKeys(Json const& _input, std::set<std::string> const& _keys, std::string const& _name)
@@ -1235,6 +1253,12 @@ Json StandardCompiler::importEVMAssembly(StandardCompiler::InputsAndSettings _in
 			creationJSON["functionDebugData"] = formatFunctionDebugData(stack.object(sourceName).functionDebugData);
 		if (evmCreationArtifactRequested("linkReferences"))
 			creationJSON["linkReferences"] = formatLinkReferences(stack.object(sourceName).linkReferences);
+		if (evmCreationArtifactRequested("subAssemblyOffsets"))
+		{
+			Json ret = Json::object();
+			ret["subs"] = formatSubAssemblyOffsets(stack.runtimeObject(sourceName).subAssemblyData);
+			creationJSON["subAssemblyOffsets"] = std::move(ret);
+		}
 		evmData["bytecode"] = creationJSON;
 	}
 
@@ -1503,6 +1527,12 @@ Json StandardCompiler::compileSolidity(StandardCompiler::InputsAndSettings _inpu
 				creationJSON["linkReferences"] = formatLinkReferences(compilerStack.object(contractName).linkReferences);
 			if (evmCreationArtifactRequested("generatedSources"))
 				creationJSON["generatedSources"] = compilerStack.generatedSources(contractName, /* _runtime */ false);
+			if (evmCreationArtifactRequested("subAssemblyOffsets"))
+			{
+				Json ret = Json::object();
+				ret["subs"] = formatSubAssemblyOffsets(compilerStack.object(contractName).subAssemblyData);
+				creationJSON["subAssemblyOffsets"] = std::move(ret);
+			}
 			evmData["bytecode"] = creationJSON;
 		}
 
@@ -1533,6 +1563,12 @@ Json StandardCompiler::compileSolidity(StandardCompiler::InputsAndSettings _inpu
 				deployedJSON["immutableReferences"] = formatImmutableReferences(compilerStack.runtimeObject(contractName).immutableReferences);
 			if (evmDeployedArtifactRequested("generatedSources"))
 				deployedJSON["generatedSources"] = compilerStack.generatedSources(contractName, /* _runtime */ true);
+			if (evmDeployedArtifactRequested("subAssemblyOffsets"))
+			{
+				Json ret = Json::object();
+				ret["subs"] = formatSubAssemblyOffsets(compilerStack.object(contractName).subAssemblyData);
+				deployedJSON["subAssemblyOffsets"] = std::move(ret);
+			}
 			evmData["deployedBytecode"] = deployedJSON;
 		}
 