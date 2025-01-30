MarketBeatAd/solidity

Enable ethdebug debug info and output selection.

test/libyul/ObjectParser.cpp
@@ -60,7 +60,7 @@ std::pair<bool, ErrorList> parse(std::string const& _source)
 		solidity::test::CommonOptions::get().eofVersion(),
 		YulStack::Language::StrictAssembly,
 		solidity::frontend::OptimiserSettings::none(),
-		DebugInfoSelection::All()
+		DebugInfoSelection::ExceptExperimental()
 	);
 	bool success = asmStack.parseAndAnalyze("source", _source);
 	return {success, asmStack.errors()};
@@ -177,7 +177,7 @@ BOOST_AUTO_TEST_CASE(to_string)
 		solidity::test::CommonOptions::get().eofVersion(),
 		YulStack::Language::StrictAssembly,
 		solidity::frontend::OptimiserSettings::none(),
-		DebugInfoSelection::All()
+		DebugInfoSelection::ExceptExperimental()
 	);
 	BOOST_REQUIRE(asmStack.parseAndAnalyze("source", code));
 	BOOST_CHECK_EQUAL(asmStack.print(), expectation);

test/tools/ossfuzz/strictasm_assembly_ossfuzz.cpp
@@ -41,7 +41,7 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t const* _data, size_t _size)
 		std::nullopt,
 		YulStack::Language::StrictAssembly,
 		solidity::frontend::OptimiserSettings::minimal(),
-		langutil::DebugInfoSelection::All()
+		langutil::DebugInfoSelection::ExceptExperimental()
 	);
 
 	if (!stack.parseAndAnalyze("source", input))

test/tools/ossfuzz/strictasm_diff_ossfuzz.cpp
@@ -65,7 +65,7 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t const* _data, size_t _size)
 		std::nullopt,
 		YulStack::Language::StrictAssembly,
 		solidity::frontend::OptimiserSettings::full(),
-		DebugInfoSelection::All()
+		DebugInfoSelection::ExceptExperimental()
 	);
 	try
 	{

ton-solidity/ton-solidity

test/libyul/ObjectParser.cpp
@@ -151,7 +151,7 @@ BOOST_AUTO_TEST_CASE(to_string)
 		solidity::test::CommonOptions::get().eofVersion(),
 		YulStack::Language::StrictAssembly,
 		solidity::frontend::OptimiserSettings::none(),
-		DebugInfoSelection::All()
+		DebugInfoSelection::AllExceptExperimental()
 	);
 	BOOST_REQUIRE(asmStack.parseAndAnalyze("source", code));
 	BOOST_CHECK_EQUAL(asmStack.print(), expectation);

test/tools/ossfuzz/strictasm_assembly_ossfuzz.cpp
@@ -41,7 +41,7 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t const* _data, size_t _size)
 		std::nullopt,
 		YulStack::Language::StrictAssembly,
 		solidity::frontend::OptimiserSettings::minimal(),
-		langutil::DebugInfoSelection::All()
+		langutil::DebugInfoSelection::AllExceptExperimental()
 	);
 
 	if (!stack.parseAndAnalyze("source", input))

test/tools/ossfuzz/strictasm_diff_ossfuzz.cpp
@@ -65,7 +65,7 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t const* _data, size_t _size)
 		std::nullopt,
 		YulStack::Language::StrictAssembly,
 		solidity::frontend::OptimiserSettings::full(),
-		DebugInfoSelection::All()
+		DebugInfoSelection::AllExceptExperimental()
 	);
 	try
 	{