463c39ac43ccf0833389988cca691effca4a2b93
Matheus Aguiar <matheus.pit@gmail.com>
2025-02-03 14:44:48 -0300

    fixup! add test

---
+++ test/libsolidity/syntaxTests/contractBaseLocation/layout_bitwise_negation_literal.sol
@@ -0,0 +1,3 @@
+contract C layout at ~0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE {}
+// ----
+// TypeError 1763: (21-88): Contract storage base location must be in range of type uint256. Current type is int_const -115...(71 digits omitted)...9935 Cannot implicitly convert signed literal to unsigned type.
