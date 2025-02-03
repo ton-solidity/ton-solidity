445acfe8be9960d39ca6a3f11c46a02994d2ab68
Matheus Aguiar <matheus.pit@gmail.com>
2025-02-03 11:53:42 -0300

    fixup! test

--- test/libsolidity/syntaxTests/contractBaseLocation/layout_with_inheritance.sol
+++ test/libsolidity/syntaxTests/contractBaseLocation/layout_with_inheritance.sol
@@ -1,7 +1,6 @@
 contract A { }
 contract B { }
+contract Z { }
 contract C is A, B layout at 0x1234 { }
-contract D layout at 0xABCD is A, B { }
+contract D layout at 0xABCD is Z { }
 // ----
-// TypeError 2031: (70-109): Conflicting storage layout specifications:Storage layout for base contract 'A' was also specified by another contract which derives from it.
-// TypeError 2031: (70-109): Conflicting storage layout specifications:Storage layout for base contract 'B' was also specified by another contract which derives from it.
