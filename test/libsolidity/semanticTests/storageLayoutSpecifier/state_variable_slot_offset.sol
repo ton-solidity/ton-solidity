db5572b798c5b9cafc8f50373483153c7cec997a
Matheus Aguiar <matheus.pit@gmail.com>
2025-02-03 16:13:59 -0300

    fixup! add test

---
+++ test/libsolidity/semanticTests/storageLayoutSpecifier/state_variable_slot_offset.sol
@@ -0,0 +1,30 @@
+contract C layout at 7 {
+    int8 x;
+    int32 y;
+    uint256 z;
+
+    function f() public returns(uint s, uint o) {
+        assembly {
+            s := x.slot
+            o := x.offset
+        }
+    }
+    function g() public returns(uint s, uint o) {
+        assembly {
+            s := y.slot
+            o := y.offset
+        }
+    }
+    function h() public returns(uint s, uint o) {
+        assembly {
+            s := z.slot
+            o := z.offset
+        }
+    }
+}
+// ====
+// EVMVersion: >=cancun
+// ----
+// f() -> 7, 0
+// g() -> 7, 1
+// h() -> 8, 0
